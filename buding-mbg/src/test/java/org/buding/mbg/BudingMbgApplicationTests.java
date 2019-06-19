7dec410acc288ca9d85f562b1c02a39ae28bec39
Godric <miaochengod@outlook.com>
2019-06-19 22:50:56 +0800

    [add] 新增buding-mbg项目

---
+++ buding-mbg/src/test/java/org/buding/mbg/BudingMbgApplicationTests.java
@@ -0,0 +1,16 @@
+package org.buding.mbg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BudingMbgApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
